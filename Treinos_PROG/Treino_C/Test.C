930b34932c6e5977ea203bdbc59f4751a7439ad8
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2024-07-30 17:31:27 -0300

    Test 🐱‍💻

---
+++ Treinos_PROG/Treino_C/Test.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main (void){
+    int i;
+    printf("Digite um nº:\n");
+    scanf ("%i", &i);
+    if (i == 1){
+    printf ("HELLO SUCK !!");
+    }else{
+        printf("BURROOOOOH, DA 0 PRA ELE !!");
+    }
+
+   return 0;
+    
+}
